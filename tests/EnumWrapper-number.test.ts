05490e5e41dd2785a66a760f7f13107329e75bbe
UselessPickles <jlau@uselesspickles.com>
2018-02-18 17:12:25 -0500

    Reorganize unit test

--- tests/EnumWrapper-number.test.ts
+++ tests/EnumWrapper-number.test.ts
@@ -34,38 +34,153 @@ describe("EnumWrapper: string enum", () => {
         expect(result1).toBe(result2);
     });
 
-    test("is ArrayLike", () => {
-        expect(enumWrapper.length).toBe(4);
-        expect(enumWrapper[0]).toEqual(["A", TestEnum.A]);
-        expect(enumWrapper[1]).toEqual(["B", TestEnum.B]);
-        expect(enumWrapper[2]).toEqual(["C", TestEnum.C]);
-        expect(enumWrapper[3]).toEqual(["D", TestEnum.D]);
+    describe("is Array-Like", () => {
+        test("length", () => {
+            expect(enumWrapper.length).toBe(4);
+        });
+
+        test("index signature", () => {
+            expect(enumWrapper[0]).toEqual(["A", TestEnum.A]);
+            expect(enumWrapper[1]).toEqual(["B", TestEnum.B]);
+            expect(enumWrapper[2]).toEqual(["C", TestEnum.C]);
+            expect(enumWrapper[3]).toEqual(["D", TestEnum.D]);
+        });
     });
 
-    test("size", () => {
-        expect(enumWrapper.size).toBe(4);
-    });
+    describe("is Map-Like", () => {
+        test("size", () => {
+            expect(enumWrapper.size).toBe(4);
+        });
 
-    test("get()", () => {
-        expect(enumWrapper.get("A")).toBe(TestEnum.A);
-        expect(enumWrapper.get("B")).toBe(TestEnum.B);
-        expect(enumWrapper.get("C")).toBe(TestEnum.C);
-        expect(enumWrapper.get("D")).toBe(TestEnum.D);
+        test("get()", () => {
+            expect(enumWrapper.get("A")).toBe(TestEnum.A);
+            expect(enumWrapper.get("B")).toBe(TestEnum.B);
+            expect(enumWrapper.get("C")).toBe(TestEnum.C);
+            expect(enumWrapper.get("D")).toBe(TestEnum.D);
 
-        expect(enumWrapper.get("blah")).toBe(undefined);
-        expect(enumWrapper.get(null)).toBe(undefined);
-        expect(enumWrapper.get(undefined)).toBe(undefined);
-    });
+            expect(enumWrapper.get("blah")).toBe(undefined);
+            expect(enumWrapper.get(null)).toBe(undefined);
+            expect(enumWrapper.get(undefined)).toBe(undefined);
+        });
 
-    test("has()", () => {
-        expect(enumWrapper.has("A")).toBe(true);
-        expect(enumWrapper.has("B")).toBe(true);
-        expect(enumWrapper.has("C")).toBe(true);
-        expect(enumWrapper.has("D")).toBe(true);
+        test("has()", () => {
+            expect(enumWrapper.has("A")).toBe(true);
+            expect(enumWrapper.has("B")).toBe(true);
+            expect(enumWrapper.has("C")).toBe(true);
+            expect(enumWrapper.has("D")).toBe(true);
 
-        expect(enumWrapper.has("blah")).toBe(false);
-        expect(enumWrapper.has(null)).toBe(false);
-        expect(enumWrapper.has(undefined)).toBe(false);
+            expect(enumWrapper.has("blah")).toBe(false);
+            expect(enumWrapper.has(null)).toBe(false);
+            expect(enumWrapper.has(undefined)).toBe(false);
+        });
+
+        test("keys()", () => {
+            // keys() returns an iterator
+            expect(enumWrapper.keys().next()).toEqual({
+                done: false,
+                value: "A"
+            });
+
+            const expected = [
+                "A",
+                "B",
+                "C",
+                "D"
+            ];
+            const result = Array.from(enumWrapper.keys());
+            expect(result).toEqual(expected);
+        });
+
+        test("values()", () => {
+            // values() returns an iterator
+            expect(enumWrapper.values().next()).toEqual({
+                done: false,
+                value: TestEnum.A
+            });
+
+            const expected = [
+                TestEnum.A,
+                TestEnum.B,
+                TestEnum.C,
+                TestEnum.D
+            ];
+            const result = Array.from(enumWrapper.values());
+            expect(result).toEqual(expected);
+        });
+
+        test("entries()", () => {
+            // entries() returns an iterator
+            expect(enumWrapper.entries().next()).toEqual({
+                done: false,
+                value: ["A", TestEnum.A]
+            });
+
+            const expected = [
+                ["A", TestEnum.A],
+                ["B", TestEnum.B],
+                ["C", TestEnum.C],
+                ["D", TestEnum.D]
+            ];
+            const result = Array.from(enumWrapper.entries());
+            expect(result).toEqual(expected);
+        });
+
+        test("@@iterator()", () => {
+            // @@iterator() returns an iterator
+            expect(enumWrapper[Symbol.iterator]().next()).toEqual({
+                done: false,
+                value: ["A", TestEnum.A]
+            });
+
+            const expected = [
+                ["A", TestEnum.A],
+                ["B", TestEnum.B],
+                ["C", TestEnum.C],
+                ["D", TestEnum.D]
+            ];
+            const result = Array.from(enumWrapper[Symbol.iterator]());
+            expect(result).toEqual(expected);
+        });
+
+        describe("forEach()", () => {
+            test("without context", () => {
+                const context = {
+                    iteratee: function(): void {
+                        expect(this).not.toBe(context);
+                    }
+                };
+
+                const iterateeSpy = jest.fn(context.iteratee);
+
+                enumWrapper.forEach(iterateeSpy);
+
+                expect(iterateeSpy.mock.calls).toEqual([
+                    [TestEnum.A, "A", TestEnum],
+                    [TestEnum.B, "B", TestEnum],
+                    [TestEnum.C, "C", TestEnum],
+                    [TestEnum.D, "D", TestEnum]
+                ]);
+            });
+
+            test("with context", () => {
+                const context = {
+                    iteratee: function(): void {
+                        expect(this).toBe(context);
+                    }
+                };
+
+                const iterateeSpy = jest.fn(context.iteratee);
+
+                enumWrapper.forEach(iterateeSpy, context);
+
+                expect(iterateeSpy.mock.calls).toEqual([
+                    [TestEnum.A, "A", TestEnum],
+                    [TestEnum.B, "B", TestEnum],
+                    [TestEnum.C, "C", TestEnum],
+                    [TestEnum.D, "D", TestEnum]
+                ]);
+            });
+        });
     });
 
     test("getKeys()", () => {
@@ -112,108 +227,6 @@ describe("EnumWrapper: string enum", () => {
         // "as any" required to bypass readonly
         (result[0][1] as any) = TestEnum.C;
         expect(enumWrapper.getEntries()).toEqual(expected);
-    });
-
-    test("keys()", () => {
-        // keys() returns an iterator
-        expect(enumWrapper.keys().next()).toEqual({
-            done: false,
-            value: "A"
-        });
-
-        const expected = [
-            "A",
-            "B",
-            "C",
-            "D"
-        ];
-        const result = Array.from(enumWrapper.keys());
-        expect(result).toEqual(expected);
-    });
-
-    test("values()", () => {
-        // values() returns an iterator
-        expect(enumWrapper.values().next()).toEqual({
-            done: false,
-            value: TestEnum.A
-        });
-
-        const expected = [
-            TestEnum.A,
-            TestEnum.B,
-            TestEnum.C,
-            TestEnum.D
-        ];
-        const result = Array.from(enumWrapper.values());
-        expect(result).toEqual(expected);
-    });
-
-    test("entries()", () => {
-        // entries() returns an iterator
-        expect(enumWrapper.entries().next()).toEqual({
-            done: false,
-            value: ["A", TestEnum.A]
-        });
-
-        const expected = [
-            ["A", TestEnum.A],
-            ["B", TestEnum.B],
-            ["C", TestEnum.C],
-            ["D", TestEnum.D]
-        ];
-        const result = Array.from(enumWrapper.entries());
-        expect(result).toEqual(expected);
-    });
-
-    test("@@iterator()", () => {
-        const expected = [
-            ["A", TestEnum.A],
-            ["B", TestEnum.B],
-            ["C", TestEnum.C],
-            ["D", TestEnum.D]
-        ];
-        const result = Array.from(enumWrapper);
-        expect(result).toEqual(expected);
-    });
-
-    describe("forEach()", () => {
-        test("without context", () => {
-            const context = {
-                iteratee: function(): void {
-                    expect(this).not.toBe(context);
-                }
-            };
-
-            const iterateeSpy = jest.fn(context.iteratee);
-
-            enumWrapper.forEach(iterateeSpy);
-
-            expect(iterateeSpy.mock.calls).toEqual([
-                [TestEnum.A, "A", TestEnum],
-                [TestEnum.B, "B", TestEnum],
-                [TestEnum.C, "C", TestEnum],
-                [TestEnum.D, "D", TestEnum]
-            ]);
-        });
-
-        test("with context", () => {
-            const context = {
-                iteratee: function(): void {
-                    expect(this).toBe(context);
-                }
-            };
-
-            const iterateeSpy = jest.fn(context.iteratee);
-
-            enumWrapper.forEach(iterateeSpy, context);
-
-            expect(iterateeSpy.mock.calls).toEqual([
-                [TestEnum.A, "A", TestEnum],
-                [TestEnum.B, "B", TestEnum],
-                [TestEnum.C, "C", TestEnum],
-                [TestEnum.D, "D", TestEnum]
-            ]);
-        });
     });
 
     describe("map()", () => {
