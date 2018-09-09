835496240aba27ed00db33bc547023d3a5a284ca
UselessPickles <jlau@uselesspickles.com>
2018-09-09 19:59:16 -0400

    Comments cleanup

--- src/$enum.ts
+++ src/$enum.ts
@@ -5,11 +5,12 @@ import { mapEnumValue } from "./mapEnumValue";
 /**
  * Map of enum object -> EnumWrapper instance.
  * Used as a cache for {@link $enum}.
- * NOTE: WeakMap has very fast lookups and avoids memory leaks if used on a temporary
- *       enum-like object. Even if a WeakMap implementation is very naiive (like a Map polyfill),
- *       lookups are plenty fast for this use case of a relatively small number of enums within
- *       a project. Just don't perform cached lookups inside tight loops when
- *       you could cache the result in a local variable, and you'll be fine :)
+ * NOTE: WeakMap has very fast lookups and avoids memory leaks if used on a
+ *       temporary enum-like object. Even if a WeakMap implementation is very
+ *       naiive (like a Map polyfill), lookups are plenty fast for this use case
+ *       of a relatively small number of enums within a project. Just don't
+ *       perform cached lookups inside tight loops when you could cache the
+ *       result in a local variable, and you'll be fine :)
  *       {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakMap}
  *       {@link https://www.measurethat.net/Benchmarks/Show/2513/5/map-keyed-by-object}
  */
@@ -39,9 +40,11 @@ export function $enum<T extends EnumLike<string, Extract<keyof T, string>>>(
     enumObj: T
 ): EnumWrapper<string, T>;
 /**
- * Gets a cached EnumWrapper for an enum-like object with a mixture of number and string values.
+ * Gets a cached EnumWrapper for an enum-like object with a mixture of number
+ * and string values.
  * Creates and caches a new EnumWrapper if one is not already cached.
- * @param enumObj - An enum-like object with a mixture of number and string values.
+ * @param enumObj - An enum-like object with a mixture of number and string
+ *        values.
  * @return An instance of EnumWrapper for the provided enumObj.
  *
  * @template T - Type of the enum-like object that is being wrapped.
@@ -49,12 +52,6 @@ export function $enum<T extends EnumLike<string, Extract<keyof T, string>>>(
 export function $enum<
     T extends EnumLike<number | string, Extract<keyof T, string>>
 >(enumObj: T): EnumWrapper<number | string, T>;
-/**
- * Gets a cached EnumWrapper for an enum-like object.
- * Creates and caches a new EnumWrapper if one is not already cached.
- * @param enumObj - An enum-like object.
- * @return An instance of EnumWrapper for the provided enumObj.
- */
 export function $enum(enumObj: object): EnumWrapper {
     let result = enumWrapperInstancesCache.get(enumObj);
 
