63bf610512590bedeb11cd62db36617695146354
fuRan <NgeKaworu@gmail.com>
2021-10-18 11:46:49 +0800

    feat: 删除不要的依赖

--- src/decorators/Select/SelectAllInject.tsx
+++ src/decorators/Select/SelectAllInject.tsx
@@ -1,8 +1,6 @@
-import ChildrenRender from '@/components/ChildrenRender';
 import { isValidValue } from '@/components/Xmiles/Search';
-import { Select, SelectProps } from 'antd';
-import { SelectValue } from 'antd/lib/select';
-import React, { cloneElement } from 'react';
+import { Select } from 'antd';
+import { cloneElement } from 'react';
 import { compose, curry, maybe } from '../utils';
 
 export interface SelectAllInjectParam {
@@ -19,55 +17,6 @@ export interface SelectAllInjectProps {
  */
 
 export default curry(
-  (param: SelectAllInjectParam, Element: ReturnType<typeof Select>) => (
-    <ChildrenRender<SelectProps<SelectValue>>>
-      {(forwardProps) => SelectAllInject(param, forwardProps, Element)}
-    </ChildrenRender>
-  ),
-);
-
-export const SelectAllInject = curry(
-  (
-    { inject }: SelectAllInjectParam,
-    forwardProps: SelectAllInjectProps,
-    Element: ReturnType<typeof Select>,
-  ) => {
-    const {
-        options,
-        value: innerValue,
-        onChange: innerChange,
-      } = Element?.props || {}, // innerProps
-      { value: outerValue, onChange: outerChange } = forwardProps,
-      value = outerValue ?? innerValue ?? [];
-
-    function injectChange(val: any[], ...args: any) {
-      // 已经选择了全选
-      const hadAll = value?.includes(inject?.value);
-      let ret = val;
-      // 有"全选", 选"任意", 清除"全选"
-      if (hadAll) {
-        ret = val.filter((v) => v !== inject?.value);
-      } else {
-        // 没有"全选" 选 "全选" 清除"其它", 否则就叠加
-        if (val?.includes(inject?.value)) {
-          ret = [inject?.value];
-        }
-      }
-      innerChange?.(ret, options);
-      outerChange?.(ret, options);
-    }
-
-    return cloneElement(Element, {
-      ...forwardProps,
-      mode: 'multiple',
-      options: isValidValue(options) ? [inject]?.concat(options) : [],
-      onChange: injectChange,
-      value,
-    });
-  },
-);
-
-export const PureSelectAllInject = curry(
   ({ inject }: SelectAllInjectParam, Element: ReturnType<typeof Select>) => {
     const { options } = Element?.props || {}; // innerProps
 
