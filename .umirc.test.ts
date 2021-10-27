02f4d2a2465d0b36142d818a3c1e3787b351e06b
ClarenceChan <tong901@163.com>
2021-10-27 10:17:38 +0800

    构建: docker 镜像

--- .umirc.test.ts
+++ .umirc.test.ts
@@ -2,11 +2,11 @@
 export default {
   define: {
     PROCESS_ENV: {
-      APP_API_BASE: 'https://marketing-dev.yingzhongshare.com',
-      APP_API_BASE_URL: 'https://marketing-dev.yingzhongshare.com',
+      APP_API_BASE: 'https://test.yingzhongshare.com',
+      APP_API_BASE_URL: 'https://test.yingzhongshare.com',
       APP_API_SECURITY: '/yingzhong-security',
-      APP_API_SECURITY_URL: 'https://marketing-dev.yingzhongshare.com',
-      APP_API_SUBSYSTEMID: 586,
+      APP_API_SECURITY_URL: 'https://test.yingzhongshare.com',
+      APP_API_SUBSYSTEMID: 1153,
       NODE_ENV: 'test',
     },
   },
