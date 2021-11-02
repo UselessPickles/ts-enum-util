58a579e6b4eb513a995345f92fbe6fc67bcd1d22
fuRan <NgeKaworu@gmail.com>
2021-11-02 16:18:27 +0800

    fix: route

--- config/routes.ts
+++ config/routes.ts
@@ -1,14 +1,7 @@
 export default [
   {
-    path: '/user',
-    component: '@/layouts/UserLayout',
-    routes: [{ path: '/user/login', component: '@/pages/User/Login' }],
-  },
-  // { path: '/user/login', component: '@/pages/User/Login' },
-  { path: '/', redirect: '/home', exact: true },
-  {
     path: '/',
-    component: '@/layouts/BasicLayout',
+    component: '@/layouts/GlobalLayout',
     routes: [
       { path: '/', redirect: '/home', exact: true },
       { path: '/user/login', component: '@/pages/User/Login' },
@@ -17,29 +10,36 @@ export default [
         component: '@/layouts/BasicLayout',
         wrappers: ['@/pages/Authorized'],
         routes: [
-          { path: '/RecommazndMananger', redirect: '/RecommandMananger/TypeMananger' },
+          { path: '/home', component: '@/pages/Home' },
+          { path: '/other', component: '@/pages/Other' },
           {
-            path: '/RecommandMananger/TypeMananger',
-            component: '@/pages/RecommandMananger/TypeMananger',
+            path: '/RecommandMananger',
+            routes: [
+              { path: '/RecommazndMananger', redirect: '/RecommandMananger/TypeMananger' },
+              {
+                path: '/RecommandMananger/TypeMananger',
+                component: '@/pages/RecommandMananger/TypeMananger',
+              },
+              {
+                path: '/RecommandMananger/homeManager',
+                component: '@/pages/RecommandMananger/homeManager',
+              },
+            ],
           },
           {
-            path: '/RecommandMananger/homeManager',
-            component: '@/pages/RecommandMananger/homeManager',
+            path: '/gameStore/mgt',
+            routes: [
+              { path: '/gameStore/mgt/', redirect: '/gameStore/mgt/test' },
+              { path: '/gameStore/mgt/:env', component: '@/pages/GameStore/mgt' },
+            ],
           },
+          {
+            path: '/hot-words',
+            component: '@/pages/HotWords',
+          },
+          { path: '/lab', component: '@/pages/Lab' },
         ],
       },
-      {
-        path: '/gameStore/mgt',
-        routes: [
-          { path: '/gameStore/mgt/', redirect: '/gameStore/mgt/test' },
-          { path: '/gameStore/mgt/:env', component: '@/pages/GameStore/mgt' },
-        ],
-      },
-      {
-        path: '/hot-words',
-        component: '@/pages/HotWords',
-      },
-      { path: '/lab', component: '@/pages/Lab' },
     ],
   },
 ];
