2a58dec53fa578c2d4c2ff95667b31d95378fb92
huangxiaowei <huangxiaowei@xmiles.cn>
2021-11-03 10:31:46 +0800

    perf: 后端接口对接

--- src/pages/RecommandMananger/homeManager/useStore.tsx
+++ src/pages/RecommandMananger/homeManager/useStore.tsx
@@ -6,6 +6,7 @@ import { Form, Modal } from 'antd';
 import { ExclamationCircleOutlined } from '@ant-design/icons';
 import { useMutation } from 'react-query';
 import RESTful from '@/utils/RESTful';
+import { add, edit } from './services';
 
 // 共享 hooks
 export function useStore() {
@@ -61,6 +62,7 @@ export function useModalFromSubmit() {
     setModalProps,
     actionRef,
     editRecord,
+    selectGame,
     setSelectGame,
     setEditRecord,
     setInputSelect,
@@ -77,15 +79,34 @@ export function useModalFromSubmit() {
     setInputSelect(undefined);
   }
 
+  const updater = useMutation((data) => edit({ data }));
+  const creater = useMutation((data) => add({ data }));
+
   function submitor() {
     return modalFormRef.validateFields().then((value) => {
+      console.log('select', selectGame);
       const { sort } = value;
       confirm({
-        title: '确认添加游戏吗？',
+        title: '确认保存游戏吗？',
         icon: <ExclamationCircleOutlined />,
         content: `首页推荐中，第【${sort}】位的游戏将更改为您配置的游戏`,
         onOk() {
-          return new Promise((resolve, reject) => {}).catch(() => console.log('Oops errors!'));
+          const { id } = editRecord,
+            gameList = selectGame?.[0];
+          value.gameName = gameList?.label;
+          value.icon = selectGame?.icon;
+
+          let data;
+          if (id) {
+            data = updater.mutateAsync({ ...value, id });
+          } else {
+            data = creater.mutateAsync(value);
+          }
+          if (!data) {
+            throw new Error('no body');
+          }
+          onCancel();
+          actionRef.current?.reload();
         },
         onCancel,
       });
