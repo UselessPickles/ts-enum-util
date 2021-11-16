9aeec336e33c250b2cdd3436a9d5e92a547342b1
NgeKaworu <NgeKaworu@163.com>
2021-11-16 11:07:27 +0000

    feat:

--- src/pages/goldCoin/mission/components/Editor/index.tsx
+++ src/pages/goldCoin/mission/components/Editor/index.tsx
@@ -1,4 +1,4 @@
-import { Form, message, Input, Modal, Switch, Space, InputNumber } from 'antd';
+import { Form, message, Input, Modal, Switch, div, InputNumber, Radio } from 'antd';
 
 import DrawerForm from '@/components/DrawerForm@latest';
 import type useDrawerForm from '@/components/DrawerForm@latest/useDrawerForm';
@@ -9,6 +9,8 @@ import isValidValue from '@/utils/isValidValue';
 import prune from '@/utils/prune';
 import { compose } from '@/decorators/utils';
 import Render from '@/decorators/Common/Render';
+import Options from '@/utils/Options';
+import { IOC } from '@/decorators/hoc';
 
 const { Item } = Form;
 
@@ -32,6 +34,17 @@ export default ({
       form.setFieldsValue({ ...formData });
     },
   });
+
+  const BtnMap = new Map([
+    [1, '开启'],
+    [0, '关闭'],
+  ]);
+
+  enum BtnEnum {
+    '关闭',
+    '开启',
+  }
+  const positiveInteger = /^[0-9]+$/;
 
   async function onSubmit() {
     try {
@@ -70,115 +83,143 @@ export default ({
       formProps={{
         ...formProps,
         onFinish: onSubmit,
-        layout: 'horizontal',
-        labelCol: {
-          span: 6,
-          style: {
-            whiteSpace: 'normal',
-          },
+        validateMessages: {
+          required: '该项不能为空',
         },
-        wrapperCol: { span: 18 },
+        initialValues: {
+          '单人单日获得金币上限数量-status': BtnEnum.关闭,
+          '单人单日获得金币总上限次数-status': BtnEnum.关闭,
+          '用户提现金额占广告行为LTV收益的比例-status': BtnEnum.关闭,
+          '单人单日总提现上限次数-status': BtnEnum.关闭,
+        },
       }}
       drawerProps={{
         ...drawerProps,
         onOk: onSubmit,
         title: '金币规则配置',
-        width: 760,
       }}
     >
       <Item name={'id'} hidden>
         <Input />
       </Item>
-      <Item
-        name={'单人单日获得金币上限数量-status'}
-        label="单人单日获得金币上限数量"
-        valuePropName="checked"
-      >
-        <Switch />
+      <Item name={'单人单日获得金币上限数量-status'} label="单人单日获得金币上限数量" required>
+        {compose<any>(
+          IOC([
+            Render((origin) => (
+              <div style={{ display: 'flex', width: '100%', gap: 8 }}>
+                <Item style={{ marginBottom: 0 }}>{origin}</Item>
+                <Item dependencies={[['单人单日获得金币上限数量-status']]} noStyle>
+                  {({ getFieldValue }) => {
+                    return (
+                      getFieldValue(['单人单日获得金币上限数量-status']) === BtnEnum.开启 && (
+                        <Item
+                          name={'单人单日获得金币上限数量'}
+                          rules={[
+                            { required: true },
+                            { pattern: positiveInteger, message: '仅允许正整数' },
+                          ]}
+                          style={{ marginBottom: 0, flex: 1 }}
+                        >
+                          <Input addonAfter="个" />
+                        </Item>
+                      )
+                    );
+                  }}
+                </Item>
+              </div>
+            )),
+          ]),
+        )(<Radio.Group optionType="button" options={Options(BtnMap).toOpt} />)}
       </Item>
-      <Item dependencies={[['单人单日获得金币上限数量-status']]} noStyle>
-        {({ getFieldValue }) =>
-          getFieldValue(['单人单日获得金币上限数量-status']) && (
-            <Item name={'单人单日获得金币上限数量'} wrapperCol={{ offset: 6 }}>
-              {compose(
-                Render((origin) => (
-                  <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>{origin}个</div>
-                )),
-              )(<InputNumber style={{ width: '100%' }} />)}
-            </Item>
-          )
-        }
+
+      <Item name={'单人单日获得金币总上限次数-status'} label="单人单日获得金币总上限次数" required>
+        {compose<any>(
+          IOC([
+            Render((origin) => (
+              <div style={{ display: 'flex', width: '100%', gap: 8 }}>
+                <Item style={{ marginBottom: 0 }}>{origin}</Item>
+                <Item dependencies={[['单人单日获得金币总上限次数-status']]} noStyle>
+                  {({ getFieldValue }) =>
+                    getFieldValue(['单人单日获得金币总上限次数-status']) === BtnEnum.开启 && (
+                      <Item
+                        name={'单人单日获得金币总上限次数'}
+                        rules={[
+                          { required: true },
+                          { pattern: positiveInteger, message: '仅允许正整数' },
+                        ]}
+                        style={{ marginBottom: 0, flex: 1 }}
+                      >
+                        <Input addonAfter="次" />
+                      </Item>
+                    )
+                  }
+                </Item>
+              </div>
+            )),
+          ]),
+        )(<Radio.Group optionType="button" options={Options(BtnMap).toOpt} />)}
       </Item>
-      <Item
-        name={'单人单日获得金币总上限次数-status'}
-        label="单人单日获得金币总上限次数"
-        valuePropName="checked"
-      >
-        <Switch />
-      </Item>
-      <Item dependencies={[['单人单日获得金币总上限次数-status']]} noStyle>
-        {({ getFieldValue }) =>
-          getFieldValue(['单人单日获得金币总上限次数-status']) && (
-            <Item name={'单人单日获得金币总上限次数'} wrapperCol={{ offset: 6 }}>
-              {compose(
-                Render((origin) => (
-                  <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>{origin}次</div>
-                )),
-              )(<InputNumber style={{ width: '100%' }} />)}
-            </Item>
-          )
-        }
-      </Item>
+
       <Item
         name={'用户提现金额占广告行为LTV收益的比例-status'}
         label="用户提现金额占广告行为LTV收益的比例"
         tooltip="用户每次提现，对用户提现金额与用广告行为与LTV收益进行对比，提现金额对比收益小于等于设置的范围，用户可提现，大于用户无法提现"
-        valuePropName="checked"
+        required
       >
-        <Switch />
+        {compose<any>(
+          IOC([
+            Render((origin) => (
+              <div style={{ display: 'flex', width: '100%', gap: 8 }}>
+                <Item style={{ marginBottom: 0 }}>{origin}</Item>
+                <Item dependencies={[['用户提现金额占广告行为LTV收益的比例-status']]} noStyle>
+                  {({ getFieldValue }) =>
+                    getFieldValue(['用户提现金额占广告行为LTV收益的比例-status']) ===
+                      BtnEnum.开启 && (
+                      <Item
+                        name={'用户提现金额占广告行为LTV收益的比例'}
+                        rules={[
+                          { required: true },
+                          { pattern: positiveInteger, message: '仅允许正整数' },
+                        ]}
+                        style={{ marginBottom: 0, flex: 1 }}
+                      >
+                        <Input addonBefore="<=" addonAfter="%" />
+                      </Item>
+                    )
+                  }
+                </Item>
+              </div>
+            )),
+          ]),
+        )(<Radio.Group optionType="button" options={Options(BtnMap).toOpt} />)}
       </Item>
-      <Item dependencies={[['用户提现金额占广告行为LTV收益的比例-status']]} noStyle>
-        {({ getFieldValue }) =>
-          getFieldValue(['用户提现金额占广告行为LTV收益的比例-status']) && (
-            <Item name={'用户提现金额占广告行为LTV收益的比例'} wrapperCol={{ offset: 6 }}>
-              {compose(
-                Render((origin) => (
-                  <div
-                    style={{ display: 'flex', alignItems: 'center', gap: 8, position: 'relative' }}
-                  >
-                    <span
-                      style={{
-                        position: 'absolute',
-                        transform: 'translate(calc(-100% - 8px), 0%)',
-                      }}
-                    >{`<=`}</span>
-                    {origin}%
-                  </div>
-                )),
-              )(<InputNumber style={{ width: '100%' }} />)}
-            </Item>
-          )
-        }
-      </Item>
-      <Item
-        name={'单人单日总提现上限次数-status'}
-        label="单人单日总提现上限次数"
-        valuePropName="checked"
-      >
-        <Switch />
-      </Item>
-      <Item dependencies={[['单人单日总提现上限次数-status']]} noStyle>
-        {({ getFieldValue }) =>
-          getFieldValue(['单人单日总提现上限次数-status']) && (
-            <Item name={'单人单日总提现上限次数'} wrapperCol={{ offset: 6 }}>
-              {compose(
-                Render((origin) => (
-                  <div style={{ display: 'flex', alignItems: 'center', gap: 8 }}>{origin}次</div>
-                )),
-              )(<InputNumber style={{ width: '100%' }} />)}
-            </Item>
-          )
-        }
+
+      <Item name={'单人单日总提现上限次数-status'} label="单人单日总提现上限次数" required>
+        {compose<any>(
+          IOC([
+            Render((origin) => (
+              <div style={{ display: 'flex', width: '100%', gap: 8 }}>
+                <Item style={{ marginBottom: 0 }}>{origin}</Item>
+                <Item dependencies={[['单人单日总提现上限次数-status']]} noStyle>
+                  {({ getFieldValue }) =>
+                    getFieldValue(['单人单日总提现上限次数-status']) === BtnEnum.开启 && (
+                      <Item
+                        name={'单人单日总提现上限次数'}
+                        rules={[
+                          { required: true },
+                          { pattern: positiveInteger, message: '仅允许正整数' },
+                        ]}
+                        style={{ marginBottom: 0, flex: 1 }}
+                      >
+                        <Input addonAfter="次" />
+                      </Item>
+                    )
+                  }
+                </Item>
+              </div>
+            )),
+          ]),
+        )(<Radio.Group optionType="button" options={Options(BtnMap).toOpt} />)}
       </Item>
     </DrawerForm>
   );
